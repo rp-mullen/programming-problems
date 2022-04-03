2b9e36d6d0c745813847adbe5e4892cc1c9b53d8
RP Mullen <rp.mullen50@gmail.com>
2022-04-02 23:37:25 -0500

    pretty binary tree generator

---
+++ prettyBinaryTreeGenerator.java
@@ -0,0 +1,70 @@
+public class prettyBinaryTreeGenerator {
+  // driver code
+  public static void main(String[] args) {
+    int N = 5;
+    prettyBinaryTreeGenerator T = new prettyBinaryTreeGenerator(N);
+    T.print();
+  }
+
+  Node root;
+  int depth;
+
+  public prettyBinaryTreeGenerator(int depth) {
+    this.root = generateTree(depth);
+  }
+
+  // Recursively generate a binary tree of fixed depth
+  private static Node generateTree(int depth) {
+    if (depth < 0) return null;
+    Node currentNode = new Node(depth);
+    if (depth > 0) {
+      currentNode.left = generateTree(depth-1);
+      currentNode.right = generateTree(depth-1);
+    }
+    return currentNode;
+  }
+
+  public Node getRoot() {
+    return root;
+  }
+
+  // Recursively traces through the tree node values, adds padding and symbolic pointers
+  public void traverse(Node n, String padding, String pointer, StringBuilder sb) {
+    if (n != null) {
+      sb.append(padding);
+      sb.append(pointer);
+      sb.append(n.data);
+      sb.append("\n");
+
+      StringBuilder paddingBuilder = new StringBuilder(padding);
+      paddingBuilder.append("| ");
+
+      String paddingBoth = paddingBuilder.toString();
+      String pointerRight = "└──";
+      String pointerLeft = (n.right != null) ? "├──" : "└──";
+
+      traverse(n.left, paddingBoth, pointerLeft, sb);
+      traverse(n.right, paddingBoth, pointerRight, sb);
+    }
+  }
+
+  // Prints the output of the Traverse method
+  public void print() {
+    StringBuilder sb = new StringBuilder();
+    traverse(root, "", "", sb);
+    System.out.println(sb.toString());
+  }
+
+}
+
+// Binary tree Node
+class Node {
+  int data;
+  Node left;
+  Node right;
+
+  Node(int i) {
+    data = i;
+  }
+
+}
