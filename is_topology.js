ce3c600017422db36bb74f9668e2414df4ba7987
Ryan Mullen <rp.mullen50@gmail.com>
2022-04-11 12:22:08 -0500

    Update is_topology.js

--- is_topology.js
+++ is_topology.js
@@ -2,66 +2,82 @@
 //is a topology on X.
 
 class Set {
-    constructor(arr) {
-        this.elems = [];
-        for (var i = 0; i < arr.length; i++) {
-            if (!this.elems.includes(arr[i])) {
-                this.elems.push(arr[i]);
-            }
-        }
-        
+    constructor() {
     }
     
     static union(S,T) {
         var U = [];
-        for (var i = 0; i < S.elems.length; i++) {
-            U.push(S.elems[i]);
+        for (var i = 0; i < S.length; i++) {
+            U.push(S[i]);
         }
-        for (var j = 0; j < T.elems.length; j++) {
-            if (!U.includes(T.elems[j])) {
-                U.push(T.elems[j]);
+        for (var j = 0; j < T.length; j++) {
+            if (!U.includes(T[j])) {
+                U.push(T[j]);
             }
         }
-        return new Set(U.sort());
+        return U.sort();
     }
     
     static intersection(S,T) {
         var I = [];
-        for (var i = 0; i < S.elems.length; i++) {
-            for (var j = 0; j < T.elems.length; j++) {
-                if (S.elems.includes(T.elems[j]) && !I.includes(T.elems[j])) {
-                    I.push(T.elems[j]);
+        for (var i = 0; i < S.length; i++) {
+            for (var j = 0; j < T.length; j++) {
+                if (S.includes(T[j]) && !I.includes(T[j])) {
+                    I.push(T[j]);
                 }
-                else if (T.elems.includes(S.elems[i]) && !I.includes(S.elems[i])) {
-                    I.push(S.elems[i]);
+                else if (T.includes(S[i]) && !I.includes(S[i])) {
+                    I.push(S[i]);
                 }
             }
         }
         
-        return new Set(I.sort());
+        return I.sort();
     }
+    
+    static equality(S,T) {
+        return S.length === T.length && S.every((val, index) => val === T[index]);
+    }
+    
+    static inclusion(A,B) {
+        var incl = false;
+        for (var i = 0; i < A.length; i++) {
+            if (this.equality(A[i],B)) {
+                incl = true;
+            }
+        }
+        return incl;
+    }
+    
 }
 
 // is T a topology of X?
 function isTopology(X,T) {
+    console.log("X: " + X);
+    console.log("T: " + stringify(T));
+    console.log("--------------------------------------------------------------------------");
+    
     let conditionOne;
+    let hasNull = false;
     // (i.) X and ∅ are both in T
-    if (T.includes(X) && T.includes(new Set([]))) {
-        console.log("Contains X and NULL set");
+    for (var i = 0; i < T.length; i++) {
+        if (T[i].length === 0) {
+            hasNull = true;
+        }
+    }
+    if (T.includes(X) && hasNull) {
         conditionOne = true;
     }
     else {
         conditionOne = false;
     }
+    console.log("Condition (i): T contains X and ∅ : true");
     
     
     // (ii.) The union of any number of sets in T is also in T
     let conditionTwo;
-    //console.log("Unions:");
     for (var i = 0; i < T.length; i++) {
         for (var j = 0; j < T.length; j++) {
-            //console.log(Set.union(T[i],T[j]));
-            if (T.includes(Set.union(T[i],T[j]))) {
+            if (Set.inclusion(T,Set.union(T[i],T[j]))) {
                 conditionTwo = true;
             }
             else {
@@ -69,12 +85,14 @@ function isTopology(X,T) {
             }
         }
     }
+    console.log("Condition (ii): Any union of sets in T is also in T : " + conditionTwo);
     
     // (iii.) the intersection of any two sets in T is alos in T
+    
     let conditionThree;
     for (var i = 0; i < T.length; i++) {
         for (var j = 0; j < T.length; j++) {
-            if (T.includes(Set.intersection(T[i],T[j]))) {
+            if (Set.inclusion(T,Set.intersection(T[i],T[j]))) {
                 conditionThree = true;
             }
             else {
@@ -82,19 +100,31 @@ function isTopology(X,T) {
             }
         }
     }
-    
-    //console.log(conditionOne);
-    //console.log(conditionTwo);
-    //console.log(conditionThree);
-    
-    return (conditionOne && conditionTwo && conditionThree);
+    console.log("Condition (iii): The intersection of any two sets in T is also in T : " + conditionThree);
+    console.log("--------------------------------------------------------------------------");
+    if (conditionOne && conditionTwo && conditionThree) {
+        console.log("Result: T is a topology on X.")
     }
+    else {
+        console.log("Result: T is NOT a topology on X.")
+    }
+}
 
+function stringify(T) {
+    var str = ""
+    for (var i = 0; i < T.length; i++) {
+        if (i === T.length-1) {
+            str += "[" + T[i].toString() + "]";
+        }
+        else {
+            str += "[" + T[i].toString() + "], ";
+        }
+    }
+    return str;
+}
 
-var nullSet = new Set([]);
-var X = new Set(['a','b','c','d','e','f']);
-var T = [X, nullSet, new Set(['a']), new Set(['c','d']), new Set(['a','c','d']), new Set(['b','c','d','e','f'])];
+var X = ['a','b','c','d','e','f'];
+var T = [X, [], ['a'], ['c','d'], ['a','c','d'], ['b','c','d','e','f']];
 
-console.log(T.includes(new Set([])));
-
+console.log("Is T a topology on X?\n");
 console.log(isTopology(X,T));
