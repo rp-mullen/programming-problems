f811839d0620e1a43d135357617f89a146560bd8
Ryan Mullen <rp.mullen50@gmail.com>
2022-04-12 14:45:51 -0500

    WIP

---
+++ dota2_senate.py
@@ -0,0 +1,30 @@
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        # if 'D', remove next 'R'
+        # if 'R', remove next 'D'
+        
+        # if 'RD', radiant
+        # if 'DR', dire
+        win = False
+        
+        while not win:
+            for i in range(len(senate)):
+                if senate[i] == 'D' and senate != 'D':
+                    try:
+                        index = senate.index('R')
+                        senate = senate[0:index :] + senate[index+1:]
+                    except:
+                        return 'Dire'
+                elif senate[i] == 'R' and senate != 'R':
+                    try:
+                        index = senate.index('D')
+                        senate = senate[0:index :] + senate[index+1:]
+                    except:
+                        return 'Radiant'
+                    
+                if senate == 'D':
+                    win = True
+                    return 'Dire'
+                if senate == 'R':
+                    win = True
+                    return 'Radiant'
