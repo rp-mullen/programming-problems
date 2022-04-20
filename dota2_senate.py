409d543a72e1c043cf5923f287021f9b2a33ac11
Ryan Mullen <rp.mullen50@gmail.com>
2022-04-20 09:41:38 -0500

    determine whether 'D' or 'R' will win

--- dota2_senate.py
+++ dota2_senate.py
@@ -1,31 +1,47 @@
-class Solution:
-    def predictPartyVictory(self, senate: str) -> str:
-        # if 'D', remove next 'R'
-        # if 'R', remove next 'D'
-        
-        # if 'RD', radiant
-        # if 'DR', dire
+
+"""
+problem: Senators are 'D'(Dire Party) or 'R' (Radiant Party). Starting in order, each senator will vote to block
+the next senator of the opposite party until only one remains. That senator determines the winner of the motion. 
+Determine from a string of senators which party will win.
+"""
+def predictPartyVictory(senate):
         win = False
+        i = 0
         
-        while not win:
-            for i in range(len(senate)):
-                if senate[i] == 'D' and senate != 'D':
-                    try:
-                        index = senate.index('R')
-                        senate = senate[0:index :] + senate[index+1:]
-                    except:
-                        return 'Dire'
-                elif senate[i] == 'R' and senate != 'R':
-                    try:
-                        index = senate.index('D')
-                        senate = senate[0:index :] + senate[index+1:]
-                    except:
-                        return 'Radiant'
-                    
-                    
-                if senate == 'D':
-                    win = True
-                    return 'Dire'
-                if senate == 'R':
-                    win = True
-                    return 'Radiant'
+        while len(senate) != 0:
+            if 'R' not in senate:
+                return 'Dire'
+            
+            if 'D' not in senate:
+                return 'Radiant'
+        
+            if i > len(senate)-1:
+                i = 0
+            print(str(i) + " : " + senate )
+            if senate[i] == 'D':
+                # check ahead first
+                if 'R' in senate[i:]:
+                    index = senate[i:].index('R')
+                    index = index + i
+                    senate = senate[0:index :] + senate[index+1:]
+                    i += 1
+                else:
+                    index = senate.index('R')
+                    senate = senate[0:index :] + senate[index+1:]
+            
+            elif senate[i] == 'R':
+                # check ahead first
+                if 'D' in senate[i:]:
+                    index = senate[i:].index('D')
+                    index = index + i
+                    senate = senate[0:index :] + senate[index+1:]
+                    i += 1
+                else:
+                    index = senate.index('D')
+                    senate = senate[0:index :] + senate[index+1:]
+                    i += 1
+                           
+senate = "DDRRR"
+# should be 'Dire'
+print(predictPartyVictory(senate))
+                
