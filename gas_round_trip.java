8a2316ec7a09beae50e97b37edd5439eeb215d41
Ryan Mullen <rp.mullen50@gmail.com>
2022-04-25 14:08:20 -0500

    (WIP)

---
+++ gas_round_trip.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int[] idxs = new int[gas.length];
+        for (int i = 0; i < gas.length; i++) {
+            idxs[i] = calculateRoundTrip(gas,cost,i);
+            
+        }
+        for (int j = 0; j < idxs.length; j++) {
+            if (idxs[j] != -1) {
+                return j;
+            }
+        }
+        return -1;
+        
+    }
+    
+    public int calculateRoundTrip(int[] gas, int[] cost, int i) {
+        int L = gas.length;
+        int idx = i;
+        int g = gas[i];
+        for (int j = 0; j < L; j++) {
+            if (i + 1 == L) {
+                i = 0;
+            }
+            g = gas[i+1] - cost[i];
+            if (g < 0) {
+                return -1;
+            }
+        }
+        return idx;
+    }
+}
